68c11e7242b61b0da98a1ed4ec7e598142c05ea6
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2024-03-12 00:12:27 +0530

    New Changes

--- src/Loops/LoopsQueOne.java
+++ src/Loops/LoopsQueOne.java
@@ -1,7 +1,18 @@
 package Loops;
 
+// Question: Print Numbers: Write a program to print numbers from 1 to 100 using for loop
 public class LoopsQueOne {
     public static void main(String[] args) {
+        System.out.println("Program to Print Numbers from 1 to 100 using For Loop");
+        for(int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
 
+        int w = 1;
+        System.out.println("Program to Print numbers from 1 to 100 using while loop");
+        while(w <= 100){
+            System.out.println(w);
+            w++;
+        }
     }
 }
