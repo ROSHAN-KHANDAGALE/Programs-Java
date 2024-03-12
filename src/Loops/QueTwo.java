45b3522e8d6977354b530758497e50592341490b
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2024-03-12 16:39:00 +0530

    Changed

---
+++ src/Loops/QueTwo.java
@@ -0,0 +1,22 @@
+package Loops;
+
+// Question => Sum of Numbers: Calculate the sum of numbers from 1 to 100 using a for loop and while loop
+public class QueTwo {
+    public static void main(String[] args) {
+        int sum=0;
+        // Using For Loop
+        for(int i=1; i<=100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+        // Using While loop
+    int i = 1;
+        while(i <= 100){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+
+    }
+}
