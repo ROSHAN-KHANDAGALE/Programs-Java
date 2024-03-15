d4f9008b62d8e3c357a6e32cc8485106d127f6bb
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2024-03-15 22:11:03 +0530

    New Change

--- src/Arrays/LargestElement.java
+++ src/Arrays/LargestElement.java
@@ -4,8 +4,12 @@ import java.util.Arrays;
 public class LargestElement {
     public static void main(String[] args) {
         int[] arr = {15, 22, 10, 12, 19};
-
-            Arrays.sort(arr);
-            System.out.println("Largest Element : " + arr[arr.length - 1]);
+        int largest = 0;
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] > largest) {
+                    largest = arr[i];
+                }
+            }
+            System.out.println("Largest Element : " + largest);
     }
 }
