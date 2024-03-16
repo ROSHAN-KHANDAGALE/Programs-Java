1f441f154800936c4d4e81e47c0a2c4336509f7b
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2024-03-16 20:54:25 +0530

    New Added

--- src/Arrays/DuplicateElements.java
+++ src/Arrays/DuplicateElements.java
@@ -1,2 +1,17 @@
-package Arrays;public class DuplicateElements {
+package Arrays;
+// Question: Duplicate Elements in Array: Print the Duplicate elements of an array.
+public class DuplicateElements {
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 11, 15, 18, 12, 10, 18, 13};
+        int duplicate = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    System.out.println("Duplicate Elements : " + arr[i]);
+                    break;
+                }
+            }
+        }
+    }
 }
