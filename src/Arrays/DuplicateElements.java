ec80cd8eefa049f6fbd1353c161efc8bf63afd57
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2024-03-16 20:55:51 +0530

    New Added

--- src/Arrays/DuplicateElements.java
+++ src/Arrays/DuplicateElements.java
@@ -3,7 +3,6 @@ package Arrays;
 public class DuplicateElements {
     public static void main(String[] args) {
         int[] arr = {10, 15, 11, 15, 18, 12, 10, 18, 13};
-        int duplicate = 0;
 
         for(int i = 0; i < arr.length; i++) {
             for(int j = i+1; j < arr.length; j++) {
