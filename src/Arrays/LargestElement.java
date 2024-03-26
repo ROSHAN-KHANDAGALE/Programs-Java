c9d9297cef7e5c7b0234d5b9974b609314a72de6
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2024-03-26 23:46:06 +0530

    Updated

--- src/Arrays/LargestElement.java
+++ src/Arrays/LargestElement.java
@@ -1,11 +1,11 @@
 package Arrays;
-import java.util.Arrays;
 // Question: Largest Element in Array: Find the largest element in an array.
 public class LargestElement {
     public static void main(String[] args) {
         int[] arr = {15, 22, 10, 12, 19};
         int largest = 0;
-            for(int i = 0; i < arr.length; i++) {
+
+            for (int i = 0; i < arr.length; i++) {
                 if(arr[i] > largest) {
                     largest = arr[i];
                 }
